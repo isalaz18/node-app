6c9dc287bcd04b5405057f92d065e1980a1f7b5b
Italia Salazar <isalaz18@stu.southtexascollege.edu>
2025-11-09 23:16:06 -0600

    create function commit

---
+++ routes/employeeRoute.js
@@ -0,0 +1,9 @@
+import express from "express"
+import {create, fetch} from "../controller/employeeController.js"
+const route = express.Router()
+
+route.post("/create", create)
+route.get("/fetch", fetch)
+
+export default route
+
