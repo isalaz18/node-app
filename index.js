3f1e73bae0afb2d991f62c8af30872dd202479ef
Italia Salazar <isalaz18@stu.southtexascollege.edu>
2025-11-09 23:22:21 -0600

    feth employees

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+import route from "./routes/employeeRoute.js"
 
 const app = express()
 app.use(bodyParser.json())
@@ -19,5 +20,6 @@ app.listen(PORT, ()=> {
     })
 
 }).catch( error => console.log(error))
+app.use("/api/employees", route)
 
 
