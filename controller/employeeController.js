b51af059fa04fc0074e41db40809f4d14411ed2c
Italia Salazar <isalaz18@stu.southtexascollege.edu>
2025-11-09 23:48:05 -0600

    find one employee

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -60,3 +60,33 @@ try {
 }
 
 }
+
+//export const deleteEmployee = async(req, res) => {
+//    try {
+//        const id = req.params.id
+ //       const employeeExist = await Employee.findOne({_id:id})  
+
+ //       if(!employeeExist) {
+  //          return res.status(404).json({message: "Employee not found"})
+ //       }
+
+  //      await Employee.findByIdAndDelete(id)
+ //       res.status(201).json({message: "Employee terminated!"})
+  //  } catch (error) {
+  //      res.status(500).json({error: "Internal Server Error"})
+   // }
+
+
+
+   export const findEmployee = async(req, res) => {
+    try {
+        const id = req.params.id
+        const employeeExist =  await Employee.findOne({_id:id})
+        if(!employeeExist) {
+            return res.status(404).json({message: "Employee does not exist "})
+        }
+        res.status(200).json(employeeExist)
+    } catch (error) {
+        res.status(500).json({error: "Internal Serve error"})
+    }
+   }
